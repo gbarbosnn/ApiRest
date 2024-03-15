4bc4ae22d6c3c687f6cc4186eff53201b05f303b
Gustavo Barbosa Nunes <gustavo.nunes@vinhosjolimont.com.br>
2024-03-15 14:01:42 -0300

    15-03-24 Middlewares validação de sessão

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # RF (Requisitos funcionais)
 
--   [ ] O usuário deve poder criar uma nova transação;
--   [ ] O usuário deve poder obter um resumo da sua conta;
--   [ ] O usuário deve poder listar todas as suas transações;
--   [ ] O usuário deve poder visualizar uma transação unica;
+-   [x] O usuário deve poder criar uma nova transação;
+-   [x] O usuário deve poder obter um resumo da sua conta;
+-   [x] O usuário deve poder listar todas as suas transações;
+-   [x] O usuário deve poder visualizar uma transação unica;
 
 # RN (Regras de negocios)
 
--   [ ] A transação pode ser do tipo crédito que somará ao valor total ou débito que subtruirá;
+-   [x] A transação pode ser do tipo crédito que somará ao valor total ou débito que subtruirá;
 -   [ ] Deve ser possivel indentificar o usuário entre as requisições;
 -   [ ] O usuário só pode visualizar transações de sua criação;
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,27 +2,9 @@ import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
 import { z } from 'zod'
 import { randomUUID } from 'crypto'
+import { checkSessionIdExists } from '../middlewares/check-sessio-id-exists'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-    app.get('/', async () => {
-        const transactions = await knex('transactions').select('*')
-        return { transactions }
-    })
-
-    app.get('/:id', async (request) => {
-        const getTransactionSchema = z.object({
-            id: z.string().uuid(),
-        })
-
-        const { id } = getTransactionSchema.parse(request.params)
-
-        const transactions = await knex('transactions')
-            .select('*')
-            .where({ id })
-            .first()
-        return { transactions }
-    })
-
     app.post('/', async (request, reply) => {
         const createTransactionSchema = z.object({
             amount: z.number(),
@@ -34,13 +16,77 @@ export async function transactionsRoutes(app: FastifyInstance) {
             request.body
         )
 
+        let sessionId = request.cookies.sessionId
+
+        if (!sessionId) {
+            sessionId = randomUUID()
+
+            reply.setCookie('sessionId', sessionId, {
+                path: '/',
+                maxAge: 60 * 60 * 24 * 7, // 1 days
+            })
+        }
+
         await knex('transactions').insert({
             id: randomUUID(),
 
             amount: type === 'debit' ? -amount : amount,
             title,
+            session_id: sessionId,
         })
 
         return reply.status(201).send()
     })
+
+    app.get(
+        '/',
+        {
+            preHandler: [checkSessionIdExists],
+        },
+        async (request) => {
+            const { sessionId } = request.cookies
+
+            const transactions = await knex('transactions')
+                .where('session_id', sessionId)
+                .select()
+            return { transactions }
+        }
+    )
+
+    app.get(
+        '/:id',
+        {
+            preHandler: [checkSessionIdExists],
+        },
+        async (request) => {
+            const { sessionId } = request.cookies
+            const getTransactionSchema = z.object({
+                id: z.string().uuid(),
+            })
+
+            const { id } = getTransactionSchema.parse(request.params)
+
+            const transactions = await knex('transactions')
+                .select('*')
+                .where({ id, session_id: sessionId })
+                .first()
+            return { transactions }
+        }
+    )
+
+    app.get(
+        '/summary',
+        {
+            preHandler: [checkSessionIdExists],
+        },
+        async (request) => {
+            const { sessionId } = request.cookies
+            const summary = await knex('transactions')
+                .where('session_id', sessionId)
+                .sum('amount', { as: 'total' })
+                .first()
+
+            return { summary }
+        }
+    )
 }
